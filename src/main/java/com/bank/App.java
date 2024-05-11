8efbf4fd567aed4d41106543063a72cb03a50c89
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-05-11 19:02:01 +0000

    get farmiliar

--- src/main/java/com/bank/App.java
+++ src/main/java/com/bank/App.java
@@ -1,38 +1,34 @@
+
+
 package com.bank;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /**
- * JavaFX App
+ * App
  */
 public class App extends Application {
 
-    private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button btn = new Button("Click me");
+        btn.setOnAction(event -> btn.setText("You clicked me!"));
 
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 1080, 720);
+
+        primaryStage.setTitle("Bank Management System");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.bank {
     requires javafx.controls;
+    requires javafx.graphics;
     requires javafx.fxml;
 
     opens com.bank to javafx.fxml;
+
     exports com.bank;
 }
